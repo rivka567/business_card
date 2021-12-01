c8baea2a147dadc317d991aac849754c7cf1f1af
Yossi <yossi@h-iMac-sl-dvora.local>
2021-12-01 14:05:40 +0200

    update

--- .history/server/dbServer_20211130212730.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test');
-
-const Cat = mongoose.model('Cat', { name: String });
-
-
-app.post('/api/user', (req, res) => {
-const kitty = new Cat({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
